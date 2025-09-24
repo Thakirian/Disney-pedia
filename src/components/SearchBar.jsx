4cf26b6b390d98d7edff307a7f3acff3d31ee342
Thaisse Kirian <thakirian@gmail.com>
2025-09-24 20:45:23 -0300

    Atualização com disparo da pesquisa

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Box, TextField, Button } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { SearchContext } from '../contexts/SearchContext'; //contexto importado
 
-function SearchBar({ termoBusca, setTermoBusca }) {
+function SearchBar() { 
+  
+  //Pegamos tudo que precisamos do contexto
+  const { termoBusca, setTermoBusca, buscarPersonagens } = useContext(SearchContext);
+
+  const handleSearch = () => {
+    buscarPersonagens(termoBusca);
+  };
+
+  const handleKeyDown = (evento) => {
+    if (evento.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', p: 2, m: 2 }}>
+    <Box 
+      component="form"
+      onSubmit={(e) => { e.preventDefault(); handleSearch(); }}
+      sx={{ display: 'flex', justifyContent: 'center', p: 2, m: 2 }}
+    >
       <TextField
         label="Buscar por personagem..."
         variant="outlined"
         value={termoBusca}
         onChange={(evento) => setTermoBusca(evento.target.value)}
+        onKeyDown={handleKeyDown} 
         sx={{ width: '50%', mr: 2 }}
       />
       <Button
+        type="submit" 
         variant="contained"
         endIcon={<SearchIcon />}
       >
